c580097df40d444eb341520a00ecf3766e358b08
Serghei <g@serghei.md>
2025-09-29 23:04:34 +0300

    telegram api

--- Handler.php
+++ Handler.php
@@ -4,22 +4,42 @@
 require_once __DIR__.'/vendor/autoload.php';
 
 try {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+
     $rawEmail = file_get_contents('php://stdin');
     $message = ZBateson\MailMimeParser\Message::from($rawEmail, true);
 
-    $rawHeaderTo = $message->getHeaderValue('to');
-    $rawHeaderFrom = $message->getHeaderValue('from');
+    $to = $message->getHeaderValue('to');
+    $from = $message->getHeaderValue('from');
     $subject = $message->getHeaderValue('subject');
     $text = $message->getTextContent();
     $html = $message->getHtmlContent();
+    $attachmentsCount = $message->getAttachmentCount();
 
     file_put_contents(__DIR__.'/out.txt', print_r([
-        'rawHeaderTo' => $rawHeaderTo,
-        'rawHeaderFrom' => $rawHeaderFrom,
+        'rawHeaderTo' => $to,
+        'rawHeaderFrom' => $from,
         'subject' => $subject,
         'text' => $text,
         'html' => $html,
     ], true));
+
+
+    $message = "📧 *New Email Received*\n";
+    $message .= "*From:* $from\n";
+    $message .= "*To:* $to\n";
+    $message .= "*Subject:* $subject\n";
+    $message .= "*Body:*\n";
+    $message .= $text ? $text : ($html ? strip_tags($html) : "[No text body]") . "\n";
+    $message .= "*Attachments:* " . $attachmentsCount . "\n";
+
+    // Telegram API request
+    $botToken = getenv('TELEGRAM_BOT_TOKEN');
+    $chatId = getenv('TELEGRAM_CHAT_ID');
+
+    $sendTextUrl = "https://api.telegram.org/bot$botToken/sendMessage";
+    file_get_contents($sendTextUrl . "?chat_id=$chatId&parse_mode=Markdown&text=" . urlencode($message));
 } catch (Throwable $t) {
     file_put_contents(__DIR__.'/out.txt', print_r([
         'error' => $t->getMessage(),
