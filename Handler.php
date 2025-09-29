2e591edbe872325d5b510b2c21d70bc68347c2f4
Serghei <g@serghei.md>
2025-09-29 22:18:13 +0300

    writing mail headers to a test file

---
+++ Handler.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+try {
+    $parser = new eXorus\PhpMimeMailParser\Parser();
+
+    $parser->setStream(fopen("php://stdin", "r"));
+
+    $rawHeaderTo = $parser->getHeader('to');
+    $rawHeaderFrom = $parser->getHeader('from');
+    $subject = $parser->getHeader('subject');
+    $text = $parser->getMessageBody('text');
+    $html = $parser->getMessageBody('html');
+
+    file_put_contents(__DIR__.'/out.txt', print_r([
+        'rawHeaderTo' => $rawHeaderTo,
+        'rawHeaderFrom' => $rawHeaderFrom,
+        'subject' => $subject,
+        'text' => $text,
+        'html' => $html,
+    ], true));
+} catch (Throwable $t) {
+    file_put_contents(__DIR__.'/out.txt', print_r([
+        'error' => $t->getMessage(),
+    ], true));
+}
+
